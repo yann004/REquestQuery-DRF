330ee476163a8494a59d48195aeeda9c575a7137
yann004 <douglasyanney0@gmail.com>
2023-11-17 14:10:44 -0400

    end

--- myApp/beyondShop/models.py
+++ myApp/beyondShop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-
+   
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
